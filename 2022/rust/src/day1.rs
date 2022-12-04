bd8fdc1f4cfe183bbfa153094a2746b339b61c81
trevorcoconnor@gmail.com <trevorcoconnor@gmail.com>
2022-12-04 01:20:02 -0500

    Adding Rust Day 1

---
+++ 2022/rust/src/day1.rs
@@ -0,0 +1,14 @@
+static FILE_PATH: &'static str = "../data/Day1.txt";
+
+pub fn day1() {
+    let contents = std::fs::read_to_string(FILE_PATH).expect("Broken file");
+    let mut summations: Vec<i32> = contents
+        .split("\n\n")
+        .map(|x| {
+            x.split("\n")
+                .map(|x| x.parse::<i32>().unwrap_or(0)).sum()
+        }).collect();
+    summations.sort_by_key(|x| -x);
+    println!("Part 1: {:?}", summations[0]);
+    println!("Part 2: {:?}", summations[..2].iter().sum::<i32>());
+}
