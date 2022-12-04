29aa29d7e498496b903cbd0e48cb25c71931b709
trevorcoconnor@gmail.com <trevorcoconnor@gmail.com>
2022-12-04 12:17:06 -0500

    Haskell Day4

---
+++ 2022/haskell/app/Day4.hs
@@ -0,0 +1,37 @@
+module Day4 (day4) where
+
+import Data.List.Split
+
+fp :: FilePath
+fp = "../data/Day4.txt"
+
+data Range = Range {lower :: Int , upper :: Int}
+
+completeOverlap :: Range -> Range -> Bool
+completeOverlap left right = compare left right || compare right left
+    where compare x y  = (lower x <= lower y) && (upper x >= upper y)
+
+
+overlap :: Range -> Range -> Bool
+overlap left right = compare left right || compare right left
+    where compare x y = lower y <= lower x && lower x <= upper y
+
+
+toRange :: String -> Range
+toRange text = Range (read x) (read y)
+    where (x: y: extra) = splitOn "-" text
+
+
+toRangePair :: String -> (Range, Range)
+toRangePair text = (toRange left, toRange right)
+    where (left: right: extra) = splitOn "," text
+
+
+day4 :: IO ()
+day4 = do
+    contents <- readFile fp
+    let pairs = map toRangePair $ lines contents
+    let part1 = length $ filter (uncurry completeOverlap) pairs
+    let part2 = length $ filter (uncurry overlap) pairs
+    putStrLn $ "Part 1: " ++ show part1
+    putStrLn $ "Part 2: " ++ show part2
--- 2022/haskell/app/Main.hs
+++ 2022/haskell/app/Main.hs
@@ -8,12 +8,14 @@ import Control.Monad
 import Day1
 import Day2
 import Day3
+import Day4
 import Data.Char (toLower)
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let args' = map (map toLower) args
+    args' <- getArgs
+    let args = map (map toLower) args'
     when ("day1" `elem` args) day1
     when ("day2" `elem` args) day2
     when ("day3" `elem` args) day3
+    when ("day4" `elem` args) day4
