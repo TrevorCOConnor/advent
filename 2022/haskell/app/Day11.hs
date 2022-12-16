2097258cd592b735bd00771604b30d11cc754fc9
trevorcoconnor@gmail.com <trevorcoconnor@gmail.com>
2022-12-15 19:35:13 -0500

    Haskell Day11 part1

--- 2022/haskell/app/Day11.hs
+++ 2022/haskell/app/Day11.hs
@@ -1,16 +1,139 @@
 module Day11 (day11) where
 
+import           Data.List.Split
+import           Text.Regex.Posix
+import qualified Data.Map as M
+import Data.List (sortOn)
+
+
+type MonkeyMap = M.Map Int (Counter Monkey)
+type Stress = Bool
+
 
 fp :: FilePath
 fp = "../data/Day11.txt"
+
+
+data Counter a = Counter Int a
+    deriving (Show)
+
+count :: Int -> Counter a -> Counter a
+count plus (Counter c a) = Counter (c + plus) a
+
+newCounter :: a -> Counter a
+newCounter = Counter 0
+
+fromCounter :: Counter a -> a
+fromCounter (Counter _ a) = a
+
+toCount :: Counter a -> Int
+toCount (Counter x _) = x
+
+
+instance Functor Counter where
+    fmap f (Counter x a) = Counter x (f a)
+
+
+data Monkey = Monkey
+    { monkeyNumber    :: Int
+    , monkeyItems     :: [Int]
+    , monkeyOperation :: Int -> Int
+    , monkeyTest      :: Int
+    , monkeyTrue      :: Int
+    , monkeyFalse     :: Int
+    }
+
+instance Show Monkey where
+    show monkey = "Monkey "
+                ++ show (monkeyNumber monkey)
+                ++ " "
+                ++ show (monkeyItems monkey)
+                ++ show (monkeyTest monkey)
+
+instance Eq Monkey where
+    m1 == m2 = monkeyNumber m1 == monkeyNumber m2
+
+
+createMonkeyMap :: [Monkey] -> MonkeyMap
+createMonkeyMap ms = M.fromList $ [(monkeyNumber m, newCounter m) | m <- ms]
+
+
+parseMonkey :: [String] -> Monkey
+parseMonkey [ monkeyLine, itemLine, operationLine
+             , testLine, trueLine, falseLine] =
+  Monkey { monkeyNumber=read $ monkeyLine =~ "[0-9]"
+         , monkeyItems=map read $ getAllTextMatches $ itemLine =~ "[0-9]+"
+         , monkeyOperation=parseOperation $ operationLine =~ "old [+*] (old|[0-9]+)"
+         , monkeyTest=read $ testLine =~ "[0-9]+"
+         , monkeyTrue=read $ trueLine =~ "[0-9]+"
+         , monkeyFalse=read $ falseLine =~ "[0-9]+"
+         }
+
+parseMonkey _ = error "Unrecognized monkey format"
+
+
+parseOperation :: String -> (Int -> Int)
+parseOperation line = applyArgument arg $ operator op
+    where
+        ["old", op, arg] = splitOn " " line
+
+        operator :: String -> (Int -> Int -> Int)
+        operator "+" = (+)
+        operator "*" = (*)
+        operator s   = error $ "Operator " ++ s ++ "not recognized"
+
+        applyArgument :: String -> (Int -> Int -> Int) -> (Int -> Int)
+        applyArgument "old" f = \x -> x `f` x
+        applyArgument num f   = (`f` read num)
+
+
+-- Turn Related Functions
+handleItem :: Stress -> Monkey -> Int -> (Int, Int)
+handleItem stress monkey item = if passedTest
+                          then (monkeyTrue monkey, newItem)
+                          else (monkeyFalse monkey, newItem)
+    where operated = monkeyOperation monkey item
+          newItem = if stress then operated else operated `div` 3
+          passedTest = newItem `mod` monkeyTest monkey == 0
+
+
+removeItemsFromMonkey :: Monkey -> Monkey
+removeItemsFromMonkey monkey = monkey { monkeyItems=[] }
+
+
+addItemToMonkey :: Int -> Monkey -> Monkey
+addItemToMonkey item monkey = monkey { monkeyItems=monkeyItems monkey ++ [item] }
+
+
+monkeyTurn :: Stress -> Counter Monkey -> MonkeyMap -> MonkeyMap
+monkeyTurn stress monkeycounter monkeyMap = foldr passItem newMap items
+    where monkey = fromCounter monkeycounter
+          items = monkeyItems monkey
+          newMap = M.adjust (count (length items) . fmap removeItemsFromMonkey)
+                            (monkeyNumber monkey) monkeyMap
+
+          passItem :: Int -> MonkeyMap -> MonkeyMap
+          passItem item mm = M.adjust (fmap (addItemToMonkey newItem)) newMonkey mm
+            where (newMonkey, newItem) = handleItem stress monkey item
+
+
+monkeyRound :: Stress -> MonkeyMap -> MonkeyMap
+monkeyRound stress mmap = foldl (\mm k -> monkeyTurn stress (mm M.! k) mm)
+                            mmap $ M.keys mmap
 
 
 day11 :: IO ()
 day11 = do
     contents <- readFile fp
     let
-        part1 = "todo"
+        monkeys = map parseMonkey $ splitOn [""] $ lines contents
+        monkeyMap = createMonkeyMap monkeys
+        part1rounds = iterate (monkeyRound False) monkeyMap
+        part2rounds = iterate (monkeyRound True) monkeyMap
+        part1 = product $ take 2 $ sortOn negate $ map toCount $ M.elems $ part1rounds !! 20
         part2 = "todo"
+        -- part2 = take 4 $ sortOn negate $ map toCount $ M.elems $ part2rounds !! 1000
+    print monkeys
     putStrLn "Day 11:"
     putStrLn $ "Day 1: " ++ show part1
     putStrLn $ "Day 2: " ++ show part2
